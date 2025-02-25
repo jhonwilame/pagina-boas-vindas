f1cf24eacc1983fef8d65d876471c29caa992d37
Jhon Wilame <72402816+jhonwilame@users.noreply.github.com>
2025-02-24 22:56:39 -0300

    padrao para puxar images do ECR

--- scripts-jhon/build.sh
+++ scripts-jhon/build.sh
@@ -1,5 +1,12 @@
-ECR_REGISTRY="SEU_REGISTRY"
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
-docker build -t repo-teste .
-docker tag repo-teste:latest $ECR_REGISTRY/repo-teste:latest
-docker push $ECR_REGISTRY/repo-teste:latest
+#!/bin/bash
+
+ECR_REGISTRY="084828595306.dkr.ecr.us-east-1.amazonaws.com"
+IMAGE_NAME="repo-teste"  # Use o mesmo nome que você deu na hora do build
+
+# Remova a linha do docker login
+
+docker build -t $IMAGE_NAME .
+docker tag $IMAGE_NAME:latest "$ECR_REGISTRY/$IMAGE_NAME:latest"
+docker push "$ECR_REGISTRY/$IMAGE_NAME:latest"
+
+echo "Build e push concluídos."
